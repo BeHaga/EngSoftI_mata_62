a52fbc0ecf6179985339499bd71d7f500b02a154
João Araújo <102301547+joao0araujo@users.noreply.github.com>
2025-07-13 12:18:07 -0300

    iniciando Devolver

--- src/br/biblioteca/command/DevolverCommand.java
+++ src/br/biblioteca/command/DevolverCommand.java
@@ -1,9 +1,32 @@
 package br.biblioteca.command;
+import java.util.List;
 
 public class DevolverCommand implements Command {
     @Override
     public void executar(String[] args) {
-        System.out.println("DevolverCommand chamado!");
-        //preciso continuar a implementação
+
+        Repositorio repo = Repositorio.getInstancia();
+        String codigoUsuario = args[0];
+        String codigoLivro = args[1];
+
+        Usuario usuario = repo.buscarUsuarioPorCodigo(codigoUsuario);
+        Livro livro = repo.buscarLivroPorCodigo(codigoLivro);
+
+        List <Emprestimo> emprestimos = usuario.getEmprestimos();
+
+        for (Emprestimo emprestimo : emprestimos) {
+            
+            if (emprestimo.getExemplar().getLivro().equals(livro)) {
+
+                //Falta a logica de devolver nas classes usuario e emprestimo
+
+                System.out.println("\nDevolução realizada: " + usuario.getNome() + " devolveu o livro: "+ livro.getTitulo());
+                
+                return;
+            }
+        }
+    
+        System.out.println("Usuario não possui emprestimos em aberto para esse livro.");
+    
     }
 }
